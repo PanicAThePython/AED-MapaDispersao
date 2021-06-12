5706fb50d96a37558320941e90808f1e7995b217
PanicAThePython <nataliaweise@gmail.com>
2021-06-12 11:38:56 -0300

    MapaDispersao almost complete

--- AED-Trab03/src/MapaDispersao.java
+++ AED-Trab03/src/MapaDispersao.java
@@ -1,37 +1,67 @@
 
 public class MapaDispersao<K, T> {
-	private Lista<K, T> tabela;
+	private Lista<K, T> tabela[];
 	
 	
 	public MapaDispersao(int qtd) {
+		int l = qtd % 32;
+		tabela = new Lista[l];
+	}
+	
+	private int calcularHash(String chave) {
 		
+		int hash = 0;
+		for (int i = 0; i < chave.length(); i++) {
+			hash = (31 * hash + chave.charAt(i)) % 32;
+		}
+		return hash;
 	}
 	
 	private int calcularHash(K chave) {
 		
-		int n = chave.hashCode()-1;
-		int h = 0;
-		
-		for (int i = 0; i < n; i++) {
-			K caractere = chave[i];
-			
-		}
-		
+		return chave.hashCode() % 32;
 	}
 	
 	public boolean inserir(K chave, T dado) {
+		int hash = calcularHash(chave);
 		
+		if (buscar(chave) != null) {
+			return false;
+		}
+		
+		tabela[hash] = (Lista<K, T>) dado;
+		return true;
 	}
 	
 	public T remover(K chave) {
-		
+		int hash = calcularHash(chave);
+		if(buscar(chave) != null) {
+			T valor = (T) tabela[hash];
+			tabela[hash] = null;
+			return valor;
+		}
+		return null;
 	}
 	
 	public T buscar(K chave) {
+		int hash = calcularHash(chave);
 		
+		for (int i = 0; i < tabela.length; i++) {
+			if (tabela[hash] == tabela[i]) {
+				return (T) tabela[i];
+			}
+		}
+		return null;
 	}
 	
 	public int quantosElementos() {
+		int qtd = 0;
 		
+		for (int i = 0; i < tabela.length; i++) {
+			if (tabela[i] != null) {
+				qtd+=1;
+			}
+		}
+		return qtd;
 	}
 }
